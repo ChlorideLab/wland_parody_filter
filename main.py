af2510cce89e5ebf2bad3e0501efc75317b1244e
ChlorideP <caclx@outlook.com>
2023-09-14 19:40:10 +0800

    format rearrangement

--- main.py
+++ main.py
@@ -16,27 +16,25 @@ logging.basicConfig(
     format='[%(asctime)s] %(levelname)s: %(message)s')
 
 with open("./config.yaml", 'r', encoding="utf-8") as cfg:
-    config: dict = yaml.load(cfg.read(), Loader=yaml.FullLoader)
-if config.get('start_page') is None:
-    config['start_page'] = 1
+    CONFIG: dict = yaml.load(cfg.read(), Loader=yaml.FullLoader)
 
 
 if __name__ == '__main__':
     parody = wland.WlandParody(
         # positional settings
-        config['domain'],
-        config['parody'],
-        config.get('adult', False))  # by default shouldn't allow R18.
+        CONFIG['domain'],
+        CONFIG['parody'],
+        # optional settings
+        CONFIG.get('adult', False))  # def not to explicit R18
 
     results = filterPages(
         parody,
-        config['start_page'],
-        # optional settings
-        config.get('end_page'),
-        tags_match=config.get('hashtags'),
-        origins_match=config.get('origins'),
-        title_match=config.get('title'),
-        negative_match=config.get('ignores'))
+        CONFIG.get('start_page', 1),  # def from 1st to last
+        CONFIG.get('end_page'),
+        tags_match=CONFIG.get('hashtags'),
+        origins_match=CONFIG.get('origins'),
+        title_match=CONFIG.get('title'),
+        negative_match=CONFIG.get('ignores'))
 
-    renderer.outputHTML(config['domain'], *results)
+    renderer.outputHTML(CONFIG['domain'], *results)
     print("Done.")
