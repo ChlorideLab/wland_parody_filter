0bf294a1a489635704b237e4e417f3001f112d98
ChlorideP <caclx@outlook.com>
2023-10-30 10:54:00 +0800

    fix: (possible) cache conflict issue 不当的next操作可能令读写指针错位，导致fetchPage无法返回正确结果。

--- wland.py
+++ wland.py
@@ -41,23 +41,21 @@ class WlandPassage(Struct):
 class _CycleCache:  # to skip when contents updated.
     def __init__(self, capacity):
         self.__lst: List[Optional[WlandPassage]] = [None] * capacity
-        self.__max = capacity
-        self.__cur = 0
+        self._max = capacity
+        self._cur = 0
 
     def store(self, elem):
-        self.__lst[self.__cur] = elem
-        self.__cur = (self.__cur + 1) % self.__max
-
-    def next(self):
-        self.__cur = (self.__cur + 1) % self.__max
+        self.__lst[self._cur] = elem
+        self._cur = (self._cur + 1) % self._max
 
     def find(self, wid: int):
-        i = 0
-        while (i < self.__max):
-            if self.__lst[i] is None or self.__lst[i].wid == wid:
-                break
-            i += 1
-        return i < self.__max and self.__lst[i] is not None
+        i, j = self._cur, 0
+        while (j < self._max and self.__lst[i] and self.__lst[i].wid != wid):
+            i = (i + 1) % self._max
+            j += 1
+        self._cur = (i if (j := j == self._max or not self.__lst[i])
+                     else i + 1) % self._max
+        return not j
 
     def toTuple(self):
         return tuple(self.__lst)
@@ -67,15 +65,14 @@ class WlandParody:
     def __init__(self, url=None, parody=None, adult=False):
         """You have to sign in before scanning.
         The user information will be automatically collected."""
-        self.url = url
-        self.parody = parody
+        self.url = f'{url}/special/{parody}'
         self.cookie = cookies.chrome(domain_name=url) if adult else None
         self._cache = _CycleCache(10)
         self._page_cached = 0
         self._page_total = 0
 
     def __repr__(self):
-        return f'{self.url}/special/{self.parody}'
+        return self.url
 
     @property
     def page_num(self):
@@ -85,22 +82,20 @@ class WlandParody:
 
     def fetchPage(self, page=1):
         if self._page_cached != page:
-            fetched = requests.get(
-                f"https://{self.url}/special/{self.parody}/page={page}",
-                cookies=self.cookie)
-            if fetched.status_code != 200:
+            response = requests.get(f"https://{self.url}/page={page}",
+                                    cookies=self.cookie)
+            if response.status_code != 200:
                 return ()
             self._page_total = int(
-                _REGEXES['_pages'].search(fetched.text).group()[3:-1])
+                _REGEXES['_pages'].search(response.text).group()[3:-1])
             self._page_cached = page
-            for i in _REGEXES['_mylist'].findall(fetched.text):
+            for i in _REGEXES['_mylist'].findall(response.text):
                 self._parse(i)
         return self._cache.toTuple()
 
     def _parse(self, dl_mylist: AnyStr):
         wid = int(_REGEXES['wid'].search(dl_mylist).group().replace('wid', ''))
         if self._cache.find(wid):
-            self._cache.next()
             return
         title = _REGEXES['title'].search(dl_mylist).group()
         if not (title := _REGEXES['HTML'].sub('', title)):
